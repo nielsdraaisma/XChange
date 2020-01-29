b0053cd931fab4e5be7047ccac0f4d6c323a48b6
Niels Draaisma <niels@draaisma.email>
2020-01-30 09:56:04 +1100

    Added coinjar getOpenOrders

--- xchange-coinjar/src/main/java/org/knowm/xchange/coinjar/service/CoinjarTradeService.java
+++ xchange-coinjar/src/main/java/org/knowm/xchange/coinjar/service/CoinjarTradeService.java
@@ -16,6 +16,7 @@ import org.knowm.xchange.coinjar.dto.CoinjarOrder;
 import org.knowm.xchange.coinjar.dto.trading.CoinjarOrderRequest;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.dto.trade.OpenOrders;
 import org.knowm.xchange.dto.trade.UserTrade;
 import org.knowm.xchange.dto.trade.UserTrades;
 import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
@@ -24,6 +25,8 @@ import org.knowm.xchange.service.trade.params.CancelOrderByIdParams;
 import org.knowm.xchange.service.trade.params.CancelOrderParams;
 import org.knowm.xchange.service.trade.params.TradeHistoryParamPaging;
 import org.knowm.xchange.service.trade.params.TradeHistoryParams;
+import org.knowm.xchange.service.trade.params.orders.OpenOrdersParamOffset;
+import org.knowm.xchange.service.trade.params.orders.OpenOrdersParams;
 
 public class CoinjarTradeService extends CoinjarTradeServiceRaw implements TradeService {
 
@@ -47,12 +50,35 @@ public class CoinjarTradeService extends CoinjarTradeServiceRaw implements Trade
     } catch (CoinjarException e) {
       throw CoinjarErrorAdapter.adaptCoinjarException(e);
     }
-  };
+  }
+
+  @Override
+  public OpenOrders getOpenOrders() throws IOException {
+    return getOpenOrders(createOpenOrdersParams());
+  }
+
+  @Override
+  public OpenOrders getOpenOrders(OpenOrdersParams params) throws IOException {
+    Integer cursor = null;
+    if (params instanceof TradeHistoryParamPaging) {
+      cursor = ((TradeHistoryParamPaging) params).getPageNumber();
+    }
+    List<LimitOrder> limitOrders =
+        getAllOrders(cursor).stream()
+            .map(CoinjarAdapters::adaptOrderToLimitOrder)
+            .collect(Collectors.toList());
+    return new OpenOrders(limitOrders);
+  }
+
+  @Override
+  public OpenOrdersParams createOpenOrdersParams() {
+    return new CoinjarOpenOrdersParams();
+  }
 
   @Override
   public Collection<Order> getOrder(String... orderIds) throws IOException {
     try {
-      String orderId = orderIds[0]; // Lists.newArrayList(orderIds).get(0);
+      String orderId = orderIds[0];
       CoinjarOrder coinjarOrder = getOrder(orderId);
       return Collections.singletonList(CoinjarAdapters.adaptOrderToLimitOrder(coinjarOrder));
     } catch (CoinjarException e) {
@@ -120,6 +146,20 @@ public class CoinjarTradeService extends CoinjarTradeServiceRaw implements Trade
     @Override
     public void setPageNumber(Integer pageNumber) {
       this.pageNumber = pageNumber;
+    }
+  }
+
+  private static class CoinjarOpenOrdersParams implements OpenOrdersParams, OpenOrdersParamOffset {
+    private Integer offset;
+
+    @Override
+    public Integer getOffset() {
+      return this.offset;
+    }
+
+    @Override
+    public void setOffset(Integer offset) {
+      this.offset = offset;
     }
   }
 }
