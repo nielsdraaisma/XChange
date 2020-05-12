6955a0f319adf33fabd1e6e2900053f484a26e0d
Niels Draaisma <cornelisd@btcx.com>
2020-05-12 10:58:31 +1000

    Added xchange-stream-btcmarkets

---
+++ xchange-stream-btcmarkets/src/main/java/info/bitrich/xchangestream/btcmarkets/dto/BTCMarketsWebSocketUnsubscribeMessage.java
@@ -0,0 +1,22 @@
+package info.bitrich.xchangestream.btcmarkets.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Lists;
+import java.util.List;
+
+public class BTCMarketsWebSocketUnsubscribeMessage {
+
+  @JsonProperty("messageType")
+  public final String messageType = "removeSubscription";
+
+  @JsonProperty("marketIds")
+  public final List<String> marketIds;
+
+  @JsonProperty("channels")
+  public final List<String> channels;
+
+  public BTCMarketsWebSocketUnsubscribeMessage(String channel, String market) {
+    this.marketIds = Lists.newArrayList(market);
+    this.channels = Lists.newArrayList(channel);
+  }
+}
