c426b4d8e81edfa665c54a76d32439834ebbc590
Niels Draaisma <niels@draaisma.email>
2021-01-01 18:32:19 +1100

    Added exception constructor for btcm

---
+++ xchange-b2c2/src/main/java/org/knowm/xchange/b2c2/dto/trade/TradeResponse.java
@@ -0,0 +1,36 @@
+package org.knowm.xchange.b2c2.dto.trade;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TradeResponse {
+  public final String created;
+  public final String instrument;
+  public final String order;
+  public final String price;
+  public final String quantity;
+  public final String rfqId;
+  public final String side;
+  public final String tradeId;
+  public final String executingUnit;
+
+  public TradeResponse(
+      @JsonProperty("created") String created,
+      @JsonProperty("instrument") String instrument,
+      @JsonProperty("order") String order,
+      @JsonProperty("price") String price,
+      @JsonProperty("quantity") String quantity,
+      @JsonProperty("rfq_id") String rfqId,
+      @JsonProperty("side") String side,
+      @JsonProperty("trade_id") String tradeId,
+      @JsonProperty("executing_unit") String executingUnit) {
+    this.created = created;
+    this.instrument = instrument;
+    this.order = order;
+    this.price = price;
+    this.quantity = quantity;
+    this.rfqId = rfqId;
+    this.side = side;
+    this.tradeId = tradeId;
+    this.executingUnit = executingUnit;
+  }
+}
--- xchange-btcmarkets/src/main/java/org/knowm/xchange/btcmarkets/dto/BTCMarketsException.java
+++ xchange-btcmarkets/src/main/java/org/knowm/xchange/btcmarkets/dto/BTCMarketsException.java
@@ -6,11 +6,12 @@ import si.mazi.rescu.HttpStatusExceptionSupport;
 
 public class BTCMarketsException extends HttpStatusExceptionSupport {
 
-  private final Boolean success;
-  private final Integer errorCode;
-  private final String clientRequestId;
-  private final Long id;
-  private final List<BTCMarketsException> responses;
+  public final Boolean success;
+  public final Integer errorCode;
+  public final String errorMessage;
+  public final String clientRequestId;
+  public final Long id;
+  public final List<BTCMarketsException> responses;
 
   public BTCMarketsException(
       @JsonProperty("success") Boolean success,
@@ -22,10 +23,25 @@ public class BTCMarketsException extends HttpStatusExceptionSupport {
     super(constructMsg(errorMessage, responses));
     this.success = success;
     this.errorCode = errorCode;
+    this.errorMessage = errorMessage;
     this.clientRequestId = clientRequestId;
     this.id = id;
     this.responses = responses;
   }
+
+  public BTCMarketsException(
+          @JsonProperty("code") String code,
+          @JsonProperty("message") String message
+  ) {
+    this.success = false;
+    this.errorCode = null;
+    this.errorMessage = code;
+    this.clientRequestId = null;
+    this.id = null;
+    this.responses = null;
+
+  }
+
 
   private static String constructMsg(String errorMessage, List<BTCMarketsException> responses) {
     final StringBuilder sb = new StringBuilder();
@@ -34,31 +50,12 @@ public class BTCMarketsException extends HttpStatusExceptionSupport {
     }
     if (responses != null) {
       for (BTCMarketsException response : responses) {
-        if (!Boolean.TRUE.equals(response.getSuccess())) {
-          sb.append(String.format("Id %d: %s", response.getId(), response.getMessage()));
+        if (!Boolean.TRUE.equals(response.success)) {
+          sb.append(String.format("Id %d: %s", response.id, response.getMessage()));
         }
       }
     }
     return sb.toString();
   }
 
-  public Boolean getSuccess() {
-    return success;
-  }
-
-  public Integer getErrorCode() {
-    return errorCode;
-  }
-
-  public String getClientRequestId() {
-    return clientRequestId;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public List<BTCMarketsException> getResponses() {
-    return responses;
-  }
 }
